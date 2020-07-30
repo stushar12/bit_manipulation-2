f99cd9d0d0337ef56e63fffa1e2908185df23057
Tushar Saxena <stushar345@gmail.com>
2020-07-31 02:38:25 +0530

    Add files via upload

---
+++ first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+  int arr[] = {4, 1, 3, 2, 1, 3, 4};
+  int n = sizeof(arr) / sizeof(arr[0]);
+  int res = arr[0];
+  for (int i = 1; i < n; i++)
+  {
+    res = res ^ arr[i];
+  }
+  printf("result is = %d", res);
+}
